2ad7472bb66e3989d29173fe31a9d263e15214ab
Meng Xu <meng.xu.cs@uwaterloo.ca>
2022-10-31 23:40:23 -0400

    x

--- language/testing-infra/bounded-fuzzing/harness/main.c
+++ language/testing-infra/bounded-fuzzing/harness/main.c
@@ -1,1 +1,10 @@
-int main(void) {}
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+extern bool process(const uint8_t *data, size_t size);
+
+int main(void) {
+  uint8_t data[0];
+  process(data, 0);
+}
--- language/testing-infra/bounded-fuzzing/src/lib.rs
+++ language/testing-infra/bounded-fuzzing/src/lib.rs
@@ -1,6 +1,15 @@
+use std::ffi::c_ulong;
+use std::slice;
+
 use move_binary_format::CompiledModule;
 use move_bytecode_verifier::verifier::verify_module;
 
-pub fn entry(binary: &[u8]) -> bool {
+/// The actual
+#[no_mangle]
+pub extern "C" fn process(bytes: *const u8, len: c_ulong) -> bool {
+    let binary = unsafe {
+        assert!(!bytes.is_null());
+        slice::from_raw_parts(bytes, len as usize)
+    };
     CompiledModule::deserialize(binary).map_or(false, |m| verify_module(&m).is_ok())
 }
